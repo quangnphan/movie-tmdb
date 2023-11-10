0f01fdd2039ab00a0448905893da2f8e6ec72b73
qp2194 <qp2194@gmail.com>
2023-11-10 13:34:42 -0600

    fix left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,11 +7,11 @@ const MainLayout = async (props: { children: React.ReactNode }) => {
   return (
     <div className="h-full relative">
       <main className="flex flex-row">
-        <div className="hidden h-full md:flex md:w-52 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
+        <div className="hidden md:flex md:w-52 md:flex-col md:inset-y-0 z-[80]">
           <LeftSidebar />
         </div>
-        <section className="flex min-h-screen flex-1 flex-col items-center md:pl-52 px-6 pb-10 pt-4 max-md:pb-32 sm:px-10">
-          <div className="w-full max-w-5xl">
+        <section className="flex min-h-screen flex-1 flex-col items-center px-6 pb-10 pt-4 max-md:pb-32 sm:px-10">
+          <div className="w-full">
             <Topbar />
             {props.children}
           </div>
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -5,6 +5,7 @@ const Topbar = () => {
   return (
     <div className="flex items-center">
       <MobileSidebar />
+      <div>Search input</div>
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/"/>
       </div>
