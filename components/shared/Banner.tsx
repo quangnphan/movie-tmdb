fac79701ef3bdeb9bd43b86e6447501fd272a830
Quang Phan <qp2194@gmail.com>
2023-11-25 00:57:59 -0600

    banner responsive

--- components/shared/Banner.tsx
+++ components/shared/Banner.tsx
@@ -25,30 +25,32 @@ interface BannerSlideProps {
 
 const Banner = ({ movies }: BannerSlideProps) => {
   return (
-    <Swiper
-    modules={[Navigation, Autoplay]}
-          navigation
-          autoplay={{ delay: 5000, disableOnInteraction: false }}
-          slidesPerView={1}
-          className="rounded-lg max-w-5xl h-[350px]"//fix this
-    >
-      {movies.map((movie: any, index: any) => (
-         <SwiperSlide key={movie.id} className="relative">
-         <Image
-           src={getImageUrl(movie.backdrop_path, "w1280")}
-           layout="fill" 
-           alt="movie banner"
-           className="object-cover"
-         />
-         <div className="absolute inset-0 flex items-center justify-center text-white">
-           <div>
-             <h1 className="text-3xl font-bold">{movie.title}</h1>
-             {/* Add other description or elements here */}
-           </div>
-         </div>
-       </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="relative md:pb-[55%] pb-[45%]">
+      <Swiper
+        modules={[Navigation, Autoplay]}
+        navigation
+        autoplay={{ delay: 5000, disableOnInteraction: false }}
+        slidesPerView={1}
+        className="!absolute !top-0 !left-0 !w-full !h-full !rounded-lg"
+      >
+        {movies.map((movie: any, index: any) => (
+          <SwiperSlide key={movie.id} className="relative">
+            <Image
+              src={getImageUrl(movie.backdrop_path, "w1280")}
+              layout="fill"
+              alt="movie banner"
+              className="object-cover top-0"
+            />
+            <div className="absolute inset-0 flex items-center justify-center text-white">
+              <div>
+                <h1 className="text-3xl font-bold">{movie.title}</h1>
+                {/* Add other description or elements here */}
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
